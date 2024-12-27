4e06698c0e2fe17fd23d1df450338392903fc421
GarvSolanki70 <technoluckyo06@gmail.com>
2024-12-27 16:44:13 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-import express from "express";
+import path from "path";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import express from "express";
 import { connect } from "mongoose";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -19,12 +22,16 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);   
 app.use("/api/messages",messageRoutes);   
-app.use("/api/users",userRoutes);   
+app.use("/api/users",userRoutes); 
 
-// app.get("/", (req,res)=>{
-//     //root route http://localhost:5000
-//     res.send("Hemlo world");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
+
 
 
 server.listen(PORT, () => {
